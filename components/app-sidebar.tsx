f14acecf308376a25b6645094d9e0eaa7ce07820
SrRata <informaticauemffb@gmail.com>
2026-04-13 10:54:00 -0500

    fix avatar error 2

--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -95,7 +95,6 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
         <NavSecondary items={data.navSecondary} className="mt-auto" />
       </SidebarContent>
       <SidebarFooter>
-        {/* <NavUser user={data.user} /> */}
       </SidebarFooter>
     </Sidebar>
   )
