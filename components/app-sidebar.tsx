925b4cd299dcb86215656cc86ad70e07ea523963
SrRata <informaticauemffb@gmail.com>
2026-04-22 11:29:38 -0500

    any

--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -21,7 +21,7 @@ import {
 } from "@tabler/icons-react"
 
 import { NavDocuments } from "@/components/nav-documents"
-import { NavMain } from "@/components/nav-main"
+// import { NavMain } from "@/components/nav-main"
 import { NavSecondary } from "@/components/nav-secondary"
 import { NavUser } from "@/components/nav-user"
 import {
